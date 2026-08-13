313eb7d2588cdb5ec8d9f3f93abc3151617e9176
llixtar <shelakov92@gmail.com>
2026-08-13 18:50:05 +0200

    feat: add Meta Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,6 +77,26 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             gtag('config', 'G-13TE38NRLV');
           `}
         </Script>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2264887891012206');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }}
+               src="https://www.facebook.com/tr?id=2264887891012206&ev=PageView&noscript=1"
+               alt=""
+          />
+        </noscript>
         <span dangerouslySetInnerHTML={{ __html: "<!-- icon created by popo2021 from Flaticon -->" }} style={{ display: "none" }} />
         {children}
       </body>
